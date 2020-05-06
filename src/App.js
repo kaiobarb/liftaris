32d19b9604bf97564321cc6a19aaa4262d1a00b6
kaiobarb <kaiobarb@gmail.com>
2020-05-06 14:50:05 -0300

    new favicon + link to blog

--- src/App.js
+++ src/App.js
@@ -12,28 +12,31 @@ class App extends React.Component {
           <div className='hero-head'></div>
           <div className='hero-body'>
             <div className='container'>
+              <div className='columns is-vcentered'>
+                <div className='column is-4'>
+                  <p class="image">
+                    <img className='is-rounded' src={logo} alt='Octomunkulus' />
+                  </p>
+                </div>
+                <div className='column is-centered'>
+                  <h1 className='title is-secondary is-size-1'>Kaio Barbosa | Liftaris</h1>
                   <div className='columns is-vcentered'>
-                    <div className='column is-4'>
-                      <p class="image">
-                        <img className='is-rounded' src={logo} alt='Octomunkulus' />
-                      </p>
+                    <div className='column is-1'>
+                      <a className='is-secondary' href='https://www.twitter.com/Liftaris1' target='blank'>Twitter</a>
                     </div>
-                    <div className='column is-centered'>
-                      <h1 className='title is-secondary is-size-1'>Kaio Barbosa | Liftaris</h1>
-                      <div className='columns'>
-                        <div className='column is-1'>
-                          <a className='is-secondary' href='https://www.twitter.com/Liftaris1' target='blank'>Twitter</a>
-                        </div>
-                        <div className='column is-1'>
-                          <a className='is-secondary' href='https://www.github.com/kaiobarb' target='blank'>Github</a>
-                        </div>
-                        <div className='column is-1'>
-                          <a className='is-secondary' href='https://www.twitch.com/Liftaris' target='blank'>Twitch</a>
-                        </div>
-                      </div>
+                    <div className='column is-1'>
+                      <a className='is-secondary' href='https://www.github.com/kaiobarb' target='blank'>Github</a>
+                    </div>
+                    <div className='column is-1'>
+                      <a className='is-secondary' href='https://www.twitch.com/Liftaris' target='blank'>Twitch</a>
+                    </div>
+                    <div className='column is-5'>
+                      <a className='is-secondary is-pulled-right is-size-4' href='https://blog.liftaris.com'><b>Blog</b></a>
                     </div>
                   </div>
- 
+                </div>
+              </div>
+
             </div>
           </div>
           <div className='hero-foot'></div>
